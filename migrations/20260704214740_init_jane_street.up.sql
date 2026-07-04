a5c6edeb9a3512d99a6ed1f664b1ad40427507d4
Oliad Rundassa <68521080+oliad1@users.noreply.github.com>
2026-07-04 19:56:45 -0400

    [SCR-11] Add Jane Street Scraper (#5)
    [SCR-11] Added Jane Street Scraper

---
+++ migrations/20260704214740_init_jane_street.up.sql
@@ -0,0 +1,12 @@
+INSERT INTO companies (
+  display_name,
+  url,
+  hex_code,
+  icon_url
+)
+VALUES (
+  'Jane Street',
+  'https://www.janestreet.com/',
+  '#FFFFFF',
+  'https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://janestreet.com/&size=128'
+);
